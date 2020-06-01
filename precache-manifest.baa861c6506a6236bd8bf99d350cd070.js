4d14797daef31c2b196b434fb6ee72fb65768193
Jana-Z <github3@jana-zeller.de>
2020-06-01 19:41:03 +0200

    Updates

---
+++ precache-manifest.baa861c6506a6236bd8bf99d350cd070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2578fbd3d7d30a51e0faa097e92707",
+    "url": "/set/index.html"
+  },
+  {
+    "revision": "c188f7b058c632f4073f",
+    "url": "/set/static/css/2.4f54bdcf.chunk.css"
+  },
+  {
+    "revision": "c188f7b058c632f4073f",
+    "url": "/set/static/js/2.5c9f46e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/set/static/js/2.5c9f46e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b94e5fcf2778f0cf31",
+    "url": "/set/static/js/main.1883a38b.chunk.js"
+  },
+  {
+    "revision": "33477fac2f11669b57b1",
+    "url": "/set/static/js/runtime-main.6484915e.js"
+  }
+]);
